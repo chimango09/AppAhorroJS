8b9ffdd482c63ebc5f1a8374fb34352f24f41a3d
chimango09 <matiasvaldi1818@gmail.com>
2025-10-12 13:32:26 -0300

    se agrega seccion de gastos y se testea funcionamiento correcto de la logica

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//--------------Ingreso monto---------------------
 let ingreso = 0
 
 const in_ingresoMensual = document.getElementById("ingreso-mensual")
@@ -29,4 +31,47 @@ function guardarIngreso(){
         cargarIngresos()
     }
 
+}
+
+//--------------Gastos---------------------
+const in_nombreGasto = document.getElementById("nombre-gasto")
+const in_montoGasto = document.getElementById("monto-gasto")
+const btn_agregarGasto = document.getElementById("agregarGasto")
+const spn_mostarGastos = document.getElementById("mostrarGasto")
+const ul_listaGastos = document.getElementById("listaGastos")
+
+
+const gastos = JSON.parse(localStorage.getItem("gastos")) || []
+
+btn_agregarGasto.addEventListener("click",agregarGasto)
+
+cargarGastos()
+
+function cargarGastos (){
+    ul_listaGastos.innerHTML = ""
+    gastos.forEach((gasto) => {
+        const liLista = document.createElement("li")
+        liLista.textContent = `${gasto.nombre}: $${gasto.monto}`
+        ul_listaGastos.appendChild(liLista)
+    })
+
+    const totalGastos = gastos.reduce((gastoAcumulado,gasto) => gastoAcumulado = gastoAcumulado + gasto.monto,0)
+    spn_mostarGastos.textContent= `$${totalGastos}`
+}
+
+function agregarGasto(){
+    const nombreGasto = in_nombreGasto.value
+    const montoGasto = parseFloat(in_montoGasto.value)
+
+    if (nombreGasto === "" || montoGasto <= 0 || isNaN(montoGasto)){
+        alert("Nombre o monto invalido, por favor revise los campos e ingrese nuevamente")
+    }  else {
+        gastos.push({
+            nombre: nombreGasto,
+            monto: montoGasto
+        })
+
+        localStorage.setItem("gastos",JSON.stringify(gastos))
+        cargarGastos()
+    }
 }
